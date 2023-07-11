47404dbd12ebf04b9d95084cf5637f9fc839c355
dawninthemoon <nuts1925@naver.com>
2023-07-11 20:30:30 +0900

    CustomGrid 수정

--- Assets/Scripts/GameMap.cs
+++ Assets/Scripts/GameMap.cs
@@ -3,62 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using RieslingUtils;
 
-public class CustomGrid<T> where T : MonoBehaviour {
-    public int Width { get; set; }
-    public int Height { get; set; }
-    public Vector3 OriginPosition { get; set; }
-    private T[,] _gridArray;
-    private float _offsetX;
-    private float _offsetY;
-
-    public CustomGrid(int width, int height, Vector3 origin, float offsetX, float offsetY) {
-        Width = width;
-        Height = height;
-        OriginPosition = origin;
-
-        _offsetX = offsetX;
-        _offsetY = offsetY;
-
-        _gridArray = new T[height, width];
-    }
-
-    public T GetElement(int row, int column) {
-        return _gridArray[row, column];
-    }
-
-    public void SetElement(int row, int column, T value) {
-        _gridArray[row, column] = value;
-    }
-
-    public Vector3 RowcolToPoint(int row, int column) {
-        float x = column * _offsetX;
-        float y = row * _offsetY;
-        return OriginPosition + new Vector3(x, y);
-    }
-
-    public List<int> GetAdjustNode(int row, int column, bool ascending) {
-        List<int> colList = new List<int>();
-        for (int x = column - 1; x <= column + 1; ++x) {
-            if (x < 0 || x >= Width) continue;
-
-            int nextRow = (ascending ? row + 1 : row - 1);
-            if (GetElement(nextRow, x)) {
-                colList.Add(x);
-            }
-        }
-        return colList;
-    }
-}
-
-public struct Rowcol {
-    public int row;
-    public int column;
-    public Rowcol(int r = 0, int c = 0) {
-        row = r;
-        column = c;
-    }
-}
-
 public class GameMap : MonoBehaviour {
     [Range(4, 7), SerializeField] private int _width = 4;
     [Range(10, 20), SerializeField] private int _height = 10;

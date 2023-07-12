38356fba013ef2cf8fd2bc73a1c79db22a3a6f8f
dawninthemoon <nuts1925@naver.com>
2023-07-12 22:10:23 +0900

    카드 시스템 틀

---
+++ Assets/Scripts/Card/CardBase.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct CardEffect {
+    public enum Type {
+        DAMAGE,
+        BLOCK,
+    }
+    public Type type;
+    public string amount;
+}
+
+[System.Serializable]
+public struct CardInfo {
+    public enum Rarity {
+        STARTER,
+        COMMON,
+        UNCOMMON,
+        RARE
+    }
+
+    public enum Type {
+        ATTACK,
+        SKILL,
+        POWER
+    }
+
+    public enum Color {
+        RED,
+        GREEN
+    }
+    
+    public string cardName;
+    public Rarity rarity;
+    public Type type;
+    public Color color;
+
+    public bool isUpgraded;
+    public bool needTarget;
+
+    public int cost;
+    public int upgradeCost;
+
+    [TextArea] public string baseDescription;
+    [TextArea] public string upgradeDescription;
+
+    public CardEffect[] baseEffects;
+    public CardEffect[] upgradeEffects;
+
+    [TextArea]
+    public string variables;
+}
+
+public class CardBase : MonoBehaviour {
+    [SerializeField] private CardInfo _cardInfo;
+    private Sprite _portrait;
+}
